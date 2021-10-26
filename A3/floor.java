55aa3eb75c7134256216122920acfd0ee353e529
ishaan20068 <78277847+ishaan20068@users.noreply.github.com>
2021-10-26 23:37:18 +0530

    Update floor.java

--- A3/floor.java
+++ A3/floor.java
@@ -3,12 +3,11 @@ import java.util.*;
 abstract class floor {
     private int number;
     private int points;
-    public int getPoints(){return this.points;};
-    public int getNumber(){return this.number;};
+    public int getPoints(){return this.points;}
+    public int getNumber(){return this.number;}
     public void setNumber(int number) {
         this.number = number;
     }
     public void setPoints(int numpoints){this.points=numpoints;}
     abstract void execute(player p);
-
 }
